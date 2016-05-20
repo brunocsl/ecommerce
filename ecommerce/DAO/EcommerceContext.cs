ef10d553be7216aa7f18b1e6bb871c9b8c550042
Bruno Carvalho <brunomarp@gmail.com>
2016-05-20 15:27:57 -0300

    Criando e Mapeando a Entidade Produtos

---
+++ ecommerce/DAO/EcommerceContext.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ecommerce.DAO
+{
+    public class EcommerceContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+  
+    }
+}
---
+++ ecommerce/Entidades/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ecommerce.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public String Nome { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+
+    }
+}
