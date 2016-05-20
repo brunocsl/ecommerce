8d40d8993a777cb39646dd2b9c5ca72b53cdedbd
Bruno Carvalho <brunomarp@gmail.com>
2016-05-20 16:30:28 -0300

    Criando CRUD de Produtos

---
+++ ecommerce/DAO/ProdutoDAO.cs
@@ -0,0 +1,45 @@
+﻿using ecommerce.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ecommerce.DAO
+{
+    public class ProdutoDAO
+    {
+        private EcommerceContext context;
+
+        public ProdutoDAO(EcommerceContext context)
+        {
+            this.context = context;
+        }
+
+        public void Adiciona(Produto produto)
+        {
+            context.Produtos.Add(produto);
+            context.SaveChanges();       
+        }
+
+        public void Atualiza()
+        {
+            context.SaveChanges();
+        }
+
+        public IList<Produto> Lista()
+        {
+            return context.Produtos.ToList();
+        }
+
+        public void Remove (Produto produto)
+        {
+            context.Produtos.Remove(produto);
+            context.SaveChanges();
+        }
+
+        public Produto BuscaPorId (int id)
+        {
+            return context.Produtos.FirstOrDefault(p => p.Id == id);
+
+    }
+}
